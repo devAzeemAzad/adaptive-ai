f3d757056a0a8d260544a03f0dc12abee7fb5afe
Azeem <Azeemazad95@gmal.com>
2026-07-28 10:40:45 +0530

    feat: connect MongoDB using Mongoose

--- services/api/src/server.js
+++ services/api/src/server.js
@@ -3,17 +3,20 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const indexRoutes = require("./routes/index.route");
+const connectDB = require("./config/database");
 
 const app = express();
 
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: "http://localhost:5173",
+  credentials: true
 }));
 const PORT = process.env.PORT || 5000;
 
 app.use("/", indexRoutes);
 app.use("/api/auth", authRoutes);
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
